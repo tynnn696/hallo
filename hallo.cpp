8a446a52a4444ef5dbb365880426ed8a4dafda11
tynnn696 <dinad2960@example.com>
2025-06-12 13:30:27 +0800

    memasukan data 5 buah nama  ke dalam array string

--- hallo.cpp
+++ hallo.cpp
@@ -1,9 +1,16 @@
-// Commit: Inisialisasi array string kosong untuk 5 nama
+// Commit: Memasukkan 5 nama ke dalam array string
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
-    string names[5]; // Array dengan 5 elemen string
+    string names[5];
+
+    names[0] = "Pepen";
+    names[1] = "Alfyan";
+    names[2] = "Aluy";
+    names[3] = "Dustyn";
+    names[4] = "Udin";
+
     return 0;
 }
